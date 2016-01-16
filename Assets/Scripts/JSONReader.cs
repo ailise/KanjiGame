da93aee225483656a89d4d5c87cc601cf78d91fd
Alicia Stott <as8885@nyu.edu>
2016-01-16 18:13:12 -0500

    First commit

---
+++ Assets/Scripts/JSONReader.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using SimpleJSON;
+using UnityEngine.UI;
+using System.Linq;
+
+public class JSONReader : MonoBehaviour
+{
+
+	public Text text;
+
+	public void Awake ()
+	{
+		string kanjiInput = System.IO.File.ReadAllText ("assets/moon_speak.json");
+
+		var parsedInput = JSON.Parse (kanjiInput);
+		Debug.Log (parsedInput.Count);
+
+		var maru = parsedInput [2] ["hiragana"];
+
+//		List<string> keyList = kanjiInput.Keys.ToList();
+		Debug.Log (maru);
+
+		text = GetComponent<Text> ();
+
+		text.text = maru [0];
+
+
+
+	}
+
+
+}
+
+
