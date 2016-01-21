f90d562cf6d3e83e39358a665fab15d485f9aa40
Alicia Stott <as8885@nyu.edu>
2016-01-21 15:37:33 -0500

    Kanji and Radicals line up and load in text boxes

--- Assets/Scripts/JSONReader.cs
+++ Assets/Scripts/JSONReader.cs
@@ -10,22 +10,31 @@ using System.Linq;
 
 public class JSONReader : MonoBehaviour
 {
-
+	public GameObject[] radicals;
 	public Text text;
+
+	public Text radical;
 
 	public void Start ()
 	{
 		string kanjiInput = System.IO.File.ReadAllText ("assets/sample_dictionary.json");
-
 		var parsedInput = JSON.Parse (kanjiInput);
-		
-	
 		var levelKanji = parsedInput [UnityEngine.Random.Range (0, parsedInput.Count)];
-		Debug.Log (levelKanji ["kanji"]);
-
+//		Debug.Log (levelKanji ["kanji"]);
 		text = GetComponent<Text> ();
-
 		text.text = levelKanji ["kanji"];  
+
+		radicals = GameObject.FindGameObjectsWithTag ("Radical");
+		var radCount = levelKanji ["radicals"].Count;
+		var i = 0;
+
+		while (i < radCount) { 
+
+			radicals [i].GetComponent<Text> ().text = levelKanji ["radicals"] [i];
+
+			i = i + 1;
+
+		}
 		
 
 
